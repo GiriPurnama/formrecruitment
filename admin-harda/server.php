8a26f5c8a7fe98693257ccb3c4cb899ca31e1f4a
GiriPurnama <hiroyuki.giri@gmail.com>
2018-04-22 12:34:19 +0700

    fixed delete

--- admin-harda/server.php
+++ admin-harda/server.php
@@ -5,21 +5,17 @@
 	  	
 	  	$load_data = mysqli_query($db, "SELECT * FROM recruitment WHERE id='$id'");
 	  	while ($row = mysqli_fetch_assoc($load_data)) {
-	  		if (is_file("upload/".$row['foto'])) {
-	  			unlink("upload/".$row['foto']);
+	  		if (is_file("upload/".$row['foto']) || ("upload/".$row['ktp']) || ("upload/".$row['ijazah']) ) {
+	  			unlink($row['foto']);
+	  			unlink($row['ktp']);
+	  			unlink($row['ijazah']);
+			  	$query_delete = mysqli_query($db, "DELETE FROM recruitment WHERE id='$id'");
+			  	if ($query_delete) {
+			  		header('location: data-user.php');
+			  	} else{
+			  		echo "gagal";
+			  	}
 	  		}
-	  		if (is_file("upload/".$row['ktp'])) {
-	  			unlink("upload/".$row['ktp']);
-	  		}
-	  		if (is_file("upload/".$row['ijazah'])) {
-	  			unlink("upload/".$row['ijazah']);
-	  		}
-	  	}
-	  	$query_delete = mysqli_query($db, "DELETE FROM recruitment WHERE id='$id'");
-	  	if ($query_delete) {
-	  		header('location: data-user.php');
-	  	} else{
-	  		echo "gagal";
 	  	}
   	}
 ?>
